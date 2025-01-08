2ed9ca8bbf2ab3b1d955e0e2ece55c4b955798f2
Hericksen <gwendal.martin35@hotmail.fr>
2025-01-08 16:00:52 +0100

    First commit clean of the project

---
+++ shell.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+/**
+ * main - Entry point for the simple shell program
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	char *input = NULL;
+	size_t input_size = 0;
+	ssize_t read_size;
+
+	(void)argc;
+	(void)argv;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
+
+		read_size = getline(&input, &input_size, stdin);
+		if (read_size == -1)
+			break;
+
+		if (input[read_size - 1] == '\n')
+			input[read_size - 1] = '\0';
+
+		if (strcmp("exit", input) == 0)
+		{
+			free(input);
+			exit(0);
+		}
+
+		execute_command(input);
+	}
+
+	free(input);
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define MAX_TOKENS 64
+
+char **tokenize_input(char *input);
+void free_args(char **args);
+char *get_command_path(char *command);
+void execute_command(char *command);
+
+#endif
