1e7a9768ceb2c2b15a964fc27fc7ffc8600db501
Hericksen <gwendal.martin35@hotmail.fr>
2025-01-09 21:49:05 +0100

    Adding every files to test properly

--- shell.h
+++ shell.h
@@ -13,7 +13,6 @@
 
 char **tokenizazion(char *input);
 void execute_command(char *command, char **env);
-void free_args(char **args);
 char *custom_getenv(const char *name, char **env);
 char *custom_which(char *command, char **env);
 char *get_command_path(char *command, char **env);
